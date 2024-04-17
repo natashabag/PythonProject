1d34e35943f8c8ae8189e55d6c68493b66c64f25
Natasha Bagramian <natalia.bagramian@gmail.com>
2024-04-17 14:52:14 -0700

    Final Version

--- pythonProject/tests/conftest.py
+++ pythonProject/tests/conftest.py
@@ -2,10 +2,11 @@ import pytest
 from selenium import webdriver
 
 
-@pytest.fixture(params=["chrome", "firefox"])
+# @pytest.fixture(params=["chrome", "firefox"])
+@pytest.fixture
 def driver(request):
-    # browser = request.config.getoption("--browser")
-    browser = request.param
+    browser = request.config.getoption("--browser")
+    #browser = request.param
     print(f"creating {browser} driver")
     if browser == "chrome":
         my_driver = webdriver.Chrome()
@@ -13,10 +14,10 @@ def driver(request):
         my_driver = webdriver.Firefox()
     else:
         raise TypeError(f"Expected 'chrome' or 'firefox' but got {browser}")
+    my_driver.implicitly_wait(10)
     yield my_driver
     print(f"closing {browser} driver")
     my_driver.quit()
-
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome", help="browser to execute tests(chrome or firefox")
